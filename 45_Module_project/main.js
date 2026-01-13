88096a0a124cdea7e24f473ccc5ff668e4d2294e
Hemaghara <hemaghara@gmail.com>
2026-01-13 11:06:46 +0530

    initial commit

---
+++ 45_Module_project/main.js
@@ -0,0 +1,7 @@
+import { add, subtract } from "./mathUtils.js";
+
+const resultadd = add(5, 3);
+console.log("5 + 3 =", resultadd);
+
+const resultsubtract=subtract(5,2);
+console.log("5 - 2 =",resultsubtract);
